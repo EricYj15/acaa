06bb8dd5dcd95b29e6cf07cb21f60bbe8dad710d
EricYj15 <84513383+EricYj15@users.noreply.github.com>
2022-06-23 13:06:08 -0300

    Add files via upload

---
+++ class.js
@@ -0,0 +1,28 @@
+class lanchoneteA {
+    lanche(carne, queijo, alface,molho, cebola, picles, pao, frango){
+        this.carne = carne;
+        this.queijo = queijo;
+        this.alface = alface;
+        this.alface = molho;
+        this.alface = cebola;
+        this.alface = picles;
+        this.alface = pao;
+        this.alface = frango;
+}
+
+}
+
+class lanchoneteB{
+    lanche(carne, queijo, alface,molho, cebola, picles, pao, frango){
+        this.carne = carne;
+        this.queijo = queijo;
+        this.alface = alface;
+        this.alface = molho;
+        this.alface = cebola;
+        this.alface = picles;
+        this.alface = pao;
+        this.alface = frango;
+    }
+}
+
+
